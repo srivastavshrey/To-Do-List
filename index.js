d2dcf91459b3ca7bd84eb27a388cabfb6786c9ba
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-06-30 12:52:13 +0530

    Added Router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { propfind } = require('./route');
 const app = express();
 const port = 8000;
-
+//use express router
+app.use('/',require('./route/index'));
 app.listen(port, function(err){
     if(err){
         console.log(`Error!:${err}`);
---
+++ route/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();// created router
+
+
+
+module.exports = router;
