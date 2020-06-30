be87a9bcc62065a96dc6b81de54732a86cd51d6c
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-06-30 13:02:33 +0530

    Added Controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+//controller to fetch data
+module.exports.home = function(req,res){
+    return res.end('Home');
+    }
--- route/index.js
+++ route/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();// created router
+const homeController = require('../controllers/home_controller'); //accessing controller  
 
-
-
+router.get('/', homeController.home);//getting data from controller
 module.exports = router;
