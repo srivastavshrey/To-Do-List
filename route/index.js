525a3b654016c13fb3502109c656de92eda55424
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-06-30 23:23:07 +0530

    CSS  changes

--- route/index.js
+++ route/index.js
@@ -6,6 +6,6 @@ const Task = require('../models/task');
 router.use(express.urlencoded());
 router.use(express.static('assets'));
 router.get('/', homeController.home);//getting data from controller
-router.post('/create-task',homeController.store);
-router.get('/delete-task/',homeController.delete);
+router.post('/create-task',homeController.store);//populating schema
+router.get('/delete-task/',homeController.delete);//deletion of data
 module.exports = router;
